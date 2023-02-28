b63e2d95a22a22ca910c8b786f9703c307bcab18
manethpak <manethpak00@gmail.com>
2023-02-28 15:10:43 +0700

    update api

--- contracts/constants/index.js
+++ contracts/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x1c7326Cb3bE5109bDb365bD20c0254f896AC9F16";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-pi-pink.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
